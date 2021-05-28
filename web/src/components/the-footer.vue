654f3dd27b4df30ae55d11d503f1f6a9bcb56c8f
delia <1987944423@qq.com>
2021-05-28 15:18:36 +0800

    4-7 制作the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    电子书 ©2021 Created by Delia
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'the-footer'
+    });
+</script>
