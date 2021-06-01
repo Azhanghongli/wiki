9bd76aff9e3e42e5c336f44bedbd73bde204eb81
delia <1987944423@qq.com>
2021-06-01 18:00:49 +0800

    6-10 增加删除电子书功能

--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -73,4 +73,11 @@ public class EbookService {
             ebookMapper.updateByPrimaryKey(ebook);
         }
     }
+
+    /**
+     * 删除
+     */
+    public void delete(Long id) {
+        ebookMapper.deleteByPrimaryKey(id);
+    }
 }
