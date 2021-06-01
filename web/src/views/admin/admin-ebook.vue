8038348fb4ed17d13d7c3efce0fb955ed8977bea
delia <1987944423@qq.com>
2021-06-01 16:23:31 +0800

    6-9 增加雪花算法工具类，完成电子书管理新增功能

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -3,6 +3,9 @@
     <a-layout-content
       :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
+      <a-button type="primary" @click="add()" size="large">
+        新增
+      </a-button>
       <a-table
         :columns="columns"
         :row-key="record => record.id"
@@ -168,6 +171,14 @@
                 ebook.value = record;
             };
 
+            /**
+             * 新增
+             */
+            const add = () => {
+                modalVisible.value = true;
+                ebook.value = {};
+            };
+
             onMounted(() => {
                 handleQuery({
                     page: 1,
@@ -183,6 +194,7 @@
                 handleTableChange,
 
                 edit,
+                add,
 
                 ebook,
                 modalVisible,
