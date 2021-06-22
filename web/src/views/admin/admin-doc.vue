eee135242705becd479c4e61f14a4ec8f54046ac
delia <1987944423@qq.com>
2021-06-22 11:07:27 +0800

    8-11 增加保存文档成功提示，编辑和新增的时候清空富文本框内容

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -205,7 +205,8 @@
                     modalLoading.value = false;
                     const data = response.data; // data = commonResp
                     if (data.success) {
-                        modalVisible.value = false;
+                        // modalVisible.value = false;
+                        message.success("保存成功！");
 
                         // 重新加载列表
                         handleQuery();
@@ -285,6 +286,8 @@
              * 编辑
              */
             const edit = (record: any) => {
+                //清空富文本框
+                editor.txt.html("");
                 modalVisible.value = true;
                 doc.value = Tool.copy(record);
                 handleQueryContent();
@@ -301,6 +304,8 @@
              * 新增
              */
             const add = () => {
+                //清空富文本框
+                editor.txt.html("");
                 modalVisible.value = true;
                 doc.value = {
                     ebookId: route.query.ebookId
