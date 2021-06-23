d08608bafb6dde15684d65bfb873ecf56b35922b
delia <1987944423@qq.com>
2021-06-23 10:53:09 +0800

    8-13 解决新增时电子书不能为空的问题

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -207,7 +207,9 @@
 
             // -------- 表单 ---------
             const doc = ref();
-            doc.value = {};
+            doc.value = {
+                ebookId: route.query.ebookId
+            };
             const modalVisible = ref(false);
             const modalLoading = ref(false);
             const editor = new E("#content");
